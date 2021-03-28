d9b2021f24f9edf9cd553125305a9cdf73a78b16
Sean V <sean.vieten@gmail.com>
2021-03-27 22:45:45 -0400

    Home Page and Routes

--- src/components/App/Routes/Routes.jsx
+++ src/components/App/Routes/Routes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from 'components/Home'
+import OtherPage from 'components/OtherPage'
 
 const RouterView = () => (
 	<Switch>
 		<Route path="/" exact component={Home} />
+		<Route path="/other-page" component={OtherPage} />
 	</Switch>
 )
 
